a04ab517858038e9ba89c3f5a5a73b587bc88341
Patryk Woziński <patryk.wozinski@gmail.com>
2017-05-16 16:15:18 +0200

    Cleanup

--- autoloader.rb
+++ autoloader.rb
@@ -3,6 +3,6 @@ require 'date'
 require_relative 'manager'
 require_relative 'deployer'
 
-Dir["services/*.rb"].each do |service|
+Dir['services/*.rb'].each do |service|
 	require_relative service
 end
